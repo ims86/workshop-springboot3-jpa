936e1d56fa23efe4e3335cc567c309bd537abecf
ismaelMS <ims.86@hotmail.com>
2023-08-14 17:00:24 -0300

    Project created

---
+++ src/main/java/com/ims/coursespring3/Coursespring3Application.java
@@ -0,0 +1,13 @@
+package com.ims.coursespring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursespring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursespring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ims/coursespring3/Coursespring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.coursespring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursespring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
