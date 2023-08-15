8250c0aba36cbd4190ff0c8855a13a2660137be2
ismaelMS <ims.86@hotmail.com>
2023-08-15 15:23:06 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/ims/coursespring3/resouces/OrderResouce.java
+++ src/main/java/com/ims/coursespring3/resouces/OrderResouce.java
@@ -1,7 +1,7 @@
 package com.ims.coursespring3.resouces;
 
-import com.ims.coursespring3.entities.User;
-import com.ims.coursespring3.services.UserService;
+import com.ims.coursespring3.entities.Order;
+import com.ims.coursespring3.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResouce {
+@RequestMapping(value = "/orders")
+public class OrderResouce {
 
     @Autowired
-    private UserService service;
+    private OrderService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
-        List<User> list = this.service.findAll();
+    public ResponseEntity<List<Order>> findAll(){
+        List<Order> list = this.service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = this.service.findById(id);
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        Order obj = this.service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 }
